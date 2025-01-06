a91194396fb3abd36964708dc75bbe30e94b83a7
Alperen Aydın <aa.aydinalperen@gmail.com>
2025-01-07 00:15:55 +0300

    Main Character Changed

---
+++ Game-Final/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    private float sensibility = 3f;
+    private float softness = 0f;
+    private float maxVerticalAngle = 5f;
+    private float minVerticalAngle = -17.25f;
+
+    private Vector2 switchPosition;
+    private Vector2 camPosition;
+
+    private GameObject playerObj;
+
+    void Start()
+    {
+        playerObj = transform.parent.gameObject;
+        camPosition = Vector2.zero;
+    }
+
+    void Update()
+    {
+        Vector2 mousePos = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        mousePos = Vector2.Scale(mousePos, new Vector2(sensibility, sensibility));
+
+        switchPosition.x = Mathf.Lerp(switchPosition.x, mousePos.x, 1f / softness);
+        switchPosition.y = Mathf.Lerp(switchPosition.y, mousePos.y, 1f / softness);
+
+        camPosition.x += switchPosition.x; // Yatay hareket (X ekseni)
+        camPosition.y = Mathf.Clamp(camPosition.y + switchPosition.y, minVerticalAngle, maxVerticalAngle);
+
+        transform.localRotation = Quaternion.Euler(-camPosition.y, 0f, 0f);
+        playerObj.transform.localRotation = Quaternion.Euler(0f, camPosition.x, 0f);
+    }
+}
---
+++ Game-Final/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    float x;
+    float y;
+    float speed = 5f;
+    float walkSpeed = 5f;
+    float runSpeed = 10f;
+    private Animator animator;
+
+    private bool canAttack = true;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
+        {
+            animator.SetBool("Walking", true);
+
+
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                speed = runSpeed;
+            }
+            else
+            {
+                speed = walkSpeed;
+            }
+        }
+        else
+        {
+            animator.SetBool("Walking", false);
+        }
+
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        x *= Time.deltaTime * speed;
+        y *= Time.deltaTime * speed;
+
+        transform.Translate(x, 0f, y);
+    }
+
+}
